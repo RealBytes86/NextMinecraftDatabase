3f5b2ff807551ed8299fcc0cc79e87c36a8536ba
RealBytes86 <echtedeveloper@gmail.com>
2023-08-29 18:51:01 +0200

    add Command Handler

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,15 @@
 import { world } from "@minecraft/server";
 
+
+console.warn("Loading world...");
+
 const setPrefix = ".";
 
 world.beforeEvents.chatSend.subscribe((ctx) => {
     const messsage = ctx.message;
     if(messsage.startsWith(setPrefix)) {
         ctx.cancel = true;
-        const args = content.slice(setPrefix.length).trim().split(/ +/g);
+        const args = messsage.slice(setPrefix.length).trim().split(/ +/g);
         const commandName = args.shift().toLowerCase() 
 
         if(commandName == "test") {
@@ -15,3 +18,5 @@ world.beforeEvents.chatSend.subscribe((ctx) => {
         }
     }
 })
+
+console.warn("Addon is Ready!");
