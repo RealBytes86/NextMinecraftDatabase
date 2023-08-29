35dcb58bd2c14e41259a66dafb00e8da080385bc
RealBytes86 <echtedeveloper@gmail.com>
2023-08-29 19:10:28 +0200

    Next

---
+++ scripts/Libs/NextMDB.js
@@ -0,0 +1,85 @@
+import { world } from "@minecraft/server";
+
+class NextMDB {
+    /**
+     * @param {string} collection 
+     */
+    constructor(collection) {
+        this.name = collection;
+    }
+
+    create() {
+        if(world.scoreboard.getObjectives().find((scoreboard) => scoreboard.displayName == this.name)) {
+            return  { response: "exists",  status: "no" };
+        } else {
+            world.scoreboard.addObjective(this.name, this.name);
+            return { response: "created",  status: "ok" };
+        }
+    }
+
+    delete() {
+        if(world.scoreboard.getObjectives().find((scoreboard) => scoreboard.displayName == this.name)) {
+            world.scoreboard.removeObjective(this.name);
+            return  { response: "exists",  status: "ok" };
+        } else {
+            return { response: "no exists",  status: "no" };
+        }
+    }
+
+    reset() {
+        if(world.scoreboard.getObjectives().find((scoreboard) => scoreboard.displayName == this.name)) {
+            let deleteCount = 0;
+            let noDeleteCount = 0;
+            try 
+            {
+                world.scoreboard.removeObjective(this.name);
+                deleteCount++;
+            } catch 
+            {
+                noDeleteCount ++;
+            }
+            try 
+            {
+                world.scoreboard.addObjective(this.name, this.name);
+                deleteCount++;
+            } catch 
+            {
+                noDeleteCount++;
+            }
+            return  { response: "reseted",  status: "ok", deleteCount: deleteCount, noDeleteCount: noDeleteCount };
+        } else {
+            return { response: "no exists",  status: "no", deleteCount: null, noDeleteCount: null };
+        }
+    }
+
+}
+
+
+/**
+ * @param {String} jsonString 
+ * @returns {Object}
+ */
+function getJson(jsonString) {
+    try {
+        const jsonParse = JSON.parse(jsonString);
+        return { json: jsonParse,}
+    }catch {
+        return { json: null }
+    }
+}
+
+/**
+ * @param {string} jsonString 
+ * @returns {Object}
+ */
+function escapeQuotes(jsonString) {
+    return jsonString.replace(/\\/g, '\\\\').replace(/"/g, '\\"');
+}
+
+/**
+ * @param {string} jsonString 
+ * @returns {Object}
+ */
+function unescapeQuotes(jsonString) {
+    return jsonString.replace(/\\"/g, '"').replace(/\\\\/g, '\\');
+}
