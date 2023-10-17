ac8f00aee4a4730ae08cea94051451603f4d44b8
RealBytes86 <echtedeveloper@gmail.com>
2023-10-17 22:57:14 +0200

    synnc

--- scripts/Example/Database.js
+++ scripts/Example/Database.js
@@ -4,7 +4,3 @@ const next = new NextMDB()
 next.deleteCollection("database")
 next.createCollection("database");
 export const database = next.collection("database");
-
-for(let i = 0; i <= 100000; i++) {
-    database.insert({hello: i});
-}
