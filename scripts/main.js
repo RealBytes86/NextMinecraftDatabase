085b8b64e9725000b3d1afcf4915918628b36f5a
RealBytes86 <echtedeveloper@gmail.com>
2023-10-18 00:57:54 +0200

    sync

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,9 @@
+import { world } from "@minecraft/server";
+import { database } from "./Example/Database";
+
 console.warn("Loading world...");
 
-import { system, world } from "@minecraft/server";
-import { database } from "./Example/Database";
 const setPrefix = ".";
-
-world.beforeEvents.itemUse.subscribe((ctx) => {
-
-    if(ctx.itemStack.typeId == "minecraft:stick") {
-        try {
-            const loc = ctx.source.getBlockFromViewDirection();
-            if(!loc) return;
-            const local = loc.block.location;
-            system.run(() => {
-                try {
-                    ctx.source.dimension.spawnEntity("minecraft:lightning_bolt", {x: local.x, y: local.y + 1, z: local.z})
-                    ctx.source.sendMessage("§aLightning successfully");
-                } catch {
-                    ctx.source.sendMessage("§cLightning failed");
-                }
-            });
-            //system.run(() => ctx.source.teleport({x: local.x, y: local.y + 1, z: local.z}, {dimension: world.getDimension(ctx.source.dimension.id)}))
-        
-        } catch {}
-    }
-})
-
 
 world.beforeEvents.chatSend.subscribe((ctx) => {
     const messsage = ctx.message;
@@ -34,12 +13,7 @@ world.beforeEvents.chatSend.subscribe((ctx) => {
         const commandName = args.shift().toLowerCase() 
 
         if(commandName == "test") {
-            console.warn(JSON.stringify(database.find({hello: 10000}).json))
-            return;
-        }
 
-        if(commandName == "test2") {
-            return;
         }
 
         ctx.sender.sendMessage("§7[§6Command§7]§r §c" + commandName + " not exist.");
