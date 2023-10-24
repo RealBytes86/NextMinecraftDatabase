0cd4d1d38057053242a32a091c9d87bd728e36d8
RealBytes86 <echtedeveloper@gmail.com>
2023-10-24 22:39:30 +0200

    sync

--- scripts/Example/Database.js
+++ scripts/Example/Database.js
@@ -4,6 +4,7 @@ const next = new NextMDB();
 export const database = next.World("database");
 database.set("Hello World", {name: "Kevin"});
 
+
 /*
 for(let i = 100000; i <= 500000; i++) {
     database.set(i.toString(), {name: i.toString()});
