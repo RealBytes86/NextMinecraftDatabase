f015d81a3e69df5313feaeb5ad220b9167e905b2
RealBytes86 <echtedeveloper@gmail.com>
2023-10-25 19:57:06 +0200

    sync

--- scripts/Libs/NextMDB.js
+++ scripts/Libs/NextMDB.js
@@ -60,9 +60,20 @@ export class NextMDB {
         this.#CONFIG.init = true;
         const dimension = world.getDimension(this.#CONFIG.dimension);
         const getBlock = dimension.getBlock(this.#CONFIG.location);
+
         if(getBlock == undefined) throw new Error("Chunk not found");
-        
         if(getBlock.typeId != "minecraft:air") dimension.fillBlocks(this.#CONFIG.location, this.#CONFIG.location, "minecraft:air");
+
+        const location = this.#CONFIG.location;
+        const response = dimension.runCommand(`tickingarea add ${location.x} ${location.y} ${location.z} ${location.x} ${location.y} ${location.z} NEXT:DATABASE`);
+
+        if(response.successCount == 0) {
+            dimension.runCommandAsync(`tickingarea remove NEXT:DATABASE`).then((response) => {
+                if(response.successCount == 1) {
+                    dimension.runCommandAsync(`tickingarea add ${location.x} ${location.y} ${location.z} ${location.x} ${location.y} ${location.z} NEXT:DATABASE`);
+                }
+            })
+        }
     }
 
     setLocationCollection({x, y, z}, dimension) {
